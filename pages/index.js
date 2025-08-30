b73acc981b8ab2eb76731278f71060bcdc7e7d0f
johnpiapian <johnpiapian@gmail.com>
2025-08-30 18:47:52 -0500

    fix: clean up

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home({article}) {   
-  
-  console.log(article);
+export default function Home({ article }) {   
   
   return (
     <div className={styles.container}>
